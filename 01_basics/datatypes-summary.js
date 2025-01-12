ff4de5daa398d7af3c7620ce6ed0fd22ecca00d5
imSaurav06 <156805591+imSaurav06@users.noreply.github.com>
2025-01-13 01:36:13 +0530

    Update datatypes-summary.js

--- 01_basics/datatypes-summary.js
+++ 01_basics/datatypes-summary.js
@@ -5,16 +5,16 @@
 const score = 100
 const scoreValue = 100.3
 
-const isLoggedIn = false
+const isLoggedIn = false ///bool
 const outsideTemp = null
-let userEmail;
+let userEmail;  //undifinde
 
-const id = Symbol('123')
+const id = Symbol('123')  //typeof == symbol
 const anotherId = Symbol('123')
 
-console.log(id === anotherId);
+console.log(id === anotherId); // false
 
-// const bigNumber = 3456543576654356754n
+// const bigNumber = 3456543576654356754n // typeof undefinde
 
 
 
@@ -22,16 +22,17 @@ console.log(id === anotherId);
 
 // Array, Objects, Functions
 
-const heros = ["shaktiman", "naagraj", "doga"];
+const heros = ["shaktiman", "naagraj", "doga"]; // typeof object
 let myObj = {
     name: "hitesh",
     age: 22,
 }
 
-const myFunction = function(){
+const myFunction = function(){  //typeof function is function
     console.log("Hello world");
 }
 
 console.log(typeof anotherId);
 
+// link for typeof
 // https://262.ecma-international.org/5.1/#sec-11.4.3
