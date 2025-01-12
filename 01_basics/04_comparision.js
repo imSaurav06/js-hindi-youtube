e92692d0d51bbcc62088445050431f21a77a3de4
imSaurav06 <156805591+imSaurav06@users.noreply.github.com>
2025-01-13 01:19:31 +0530

    Update 04_comparision.js

--- 01_basics/04_comparision.js
+++ 01_basics/04_comparision.js
@@ -1,21 +1,22 @@
-// console.log(2 > 1);
-// console.log(2 >= 1);
-// console.log(2 < 1);
-// console.log(2 == 1);
-// console.log(2 != 1);
+// console.log(2 > 1); //true
+// console.log(2 >= 1); //true
+// console.log(2 < 1);  //false
+// console.log(2 == 1); //false
+// console.log(2 != 1); // true
 
 
-// console.log("2" > 1);
-// console.log("02" > 1);
+// console.log("2" > 1);  // true
+// console.log("02" > 1); // true
+// === strictly check
 
-console.log(null > 0);
-console.log(null == 0);
-console.log(null >= 0);
+console.log("2" === 2); // false
 
-console.log(undefined == 0);
-console.log(undefined > 0);
-console.log(undefined < 0);
 
-// === 
+console.log(null > 0); // false
+console.log(null == 0);  // false
+console.log(null >= 0); // true because null convert in numbers
 
-console.log("2" === 2);
+console.log(undefined == 0); // false 
+console.log(undefined > 0); // false
+console.log(undefined < 0); // false
+
