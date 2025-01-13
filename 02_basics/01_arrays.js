c1c595a728ee16472fbc787e7954012e2d5e0a08
imSaurav06 <156805591+imSaurav06@users.noreply.github.com>
2025-01-14 02:45:55 +0530

    Update 01_arrays.js

--- 02_basics/01_arrays.js
+++ 02_basics/01_arrays.js
@@ -12,28 +12,29 @@ const myArr2 = new Array(1, 2, 3, 4)
 // myArr.push(7)
 // myArr.pop()
 
-// myArr.unshift(9)
-// myArr.shift()
+// myArr.unshift(9) //add at star ---- [9,0,1,2,3,4,5]
+// myArr.shift()  // removed 9 ------[0,1,2,3,4,5]
 
-// console.log(myArr.includes(9));
-// console.log(myArr.indexOf(3));
+// console.log(myArr.includes(9)); // available or not -- false
+// console.log(myArr.indexOf(3)); -- 3
 
-// const newArr = myArr.join()
+// const newArr = myArr.join()  // change the array into string 
 
-// console.log(myArr);
-// console.log( newArr);
+// console.log(myArr);  // typeof object
+// console.log( newArr); // typeof string
 
 
 // slice, splice
 
 console.log("A ", myArr);
 
-const myn1 = myArr.slice(1, 3)
+const myn1 = myArr.slice(1, 3)  // return the copy of section of array ---[1,2] < 3
 
 console.log(myn1);
 console.log("B ", myArr);
 
 
-const myn2 = myArr.splice(1, 3)
-console.log("C ", myArr);
-console.log(myn2);
+const myn2 = myArr.splice(1, 3)  // remove the section of in oroginal array ---[0,4,5]
+console.log("C ", myArr);  //   oroginal array ---[0,4,5]
+console.log(myn2);    // [1,2,3]  output of splice
+ 
