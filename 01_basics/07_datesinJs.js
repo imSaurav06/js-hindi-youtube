df4a5655181e4821bc650e891c6d33771babfac8
imSaurav06 <156805591+imSaurav06@users.noreply.github.com>
2025-01-14 02:19:23 +0530

    Update 07_datesinJs.js

--- 01_basics/07_datesinJs.js
+++ 01_basics/07_datesinJs.js
@@ -1,24 +1,24 @@
 // Dates
 
-let myDate = new Date()
-// console.log(myDate.toString());
-// console.log(myDate.toDateString());
-// console.log(myDate.toLocaleString());
+let myDate = new Date()   // typeof - object
+// console.log(myDate.toString());         // Tue Jan 14 2025 01:52:13 GMT+0530 (India Standard Time)
+// console.log(myDate.toDateString());        // Tue Jan 14 2025
+// console.log(myDate.toLocaleString());      // 1/14/2025, 1:52:13 AM
 // console.log(typeof myDate);
 
-// let myCreatedDate = new Date(2023, 0, 23)
-// let myCreatedDate = new Date(2023, 0, 23, 5, 3)
-// let myCreatedDate = new Date("2023-01-14")
+// let myCreatedDate = new Date(2023, 0, 23)  //yyyy , mm , dd
+// let myCreatedDate = new Date(2023, 0, 23, 5, 3) //yyyy , mm , dd , tt , ss
+// let myCreatedDate = new Date("2023-01-14") // yyyy mm dd
 let myCreatedDate = new Date("01-14-2023")
 // console.log(myCreatedDate.toLocaleString());
 
-let myTimeStamp = Date.now()
+let myTimeStamp = Date.now()   /// milli_seconds
 
 // console.log(myTimeStamp);
 // console.log(myCreatedDate.getTime());
-// console.log(Math.floor(Date.now()/1000));
+// console.log(Math.floor(Date.now()/1000)); /// milli_seconds into second
 
-let newDate = new Date()
+let newDate = new Date()  
 console.log(newDate);
 console.log(newDate.getMonth() + 1);
 console.log(newDate.getDay());
