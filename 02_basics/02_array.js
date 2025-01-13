ea75c9bd5637c4d5b7874b22251d76d120419841
imSaurav06 <156805591+imSaurav06@users.noreply.github.com>
2025-01-14 03:07:34 +0530

    Update 02_array.js

--- 02_basics/02_array.js
+++ 02_basics/02_array.js
@@ -6,23 +6,23 @@ const dc_heros = ["superman", "flash", "batman"]
 // console.log(marvel_heros);
 // console.log(marvel_heros[3][1]);
 
-// const allHeros = marvel_heros.concat(dc_heros)
+// const allHeros = marvel_heros.concat(dc_heros)  // ["thor", "Ironman", "spiderman","superman", "flash", "batman"]
 // console.log(allHeros);
 
-const all_new_heros = [...marvel_heros, ...dc_heros]
+const all_new_heros = [...marvel_heros, ...dc_heros]  //  ["thor", "Ironman", "spiderman","superman", "flash", "batman"]  best
 
 // console.log(all_new_heros);
 
 const another_array = [1, 2, 3, [4, 5, 6], 7, [6, 7, [4, 5]]]
 
-const real_another_array = another_array.flat(Infinity)
+const real_another_array = another_array.flat(Infinity) // add all eliment in one arr[]
 console.log(real_another_array);
 
 
 
-console.log(Array.isArray("Hitesh"))
-console.log(Array.from("Hitesh"))
-console.log(Array.from({name: "hitesh"})) // interesting
+console.log(Array.isArray("Hitesh")) // false not an array
+console.log(Array.from("Hitesh")) // convert in arr ['H','i','t','e','s','h']
+console.log(Array.from({name: "hitesh"})) // interesting --- empty arr[]
 
 let score1 = 100
 let score2 = 200
