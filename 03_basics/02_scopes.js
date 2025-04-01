8c5dfe2ef519b50ad534e9ce68a91132b9084d20
imSaurav06 <156805591+imSaurav06@users.noreply.github.com>
2025-04-01 23:26:10 +0530

    Update 02_scopes.js

--- 03_basics/02_scopes.js
+++ 03_basics/02_scopes.js
@@ -1,3 +1,20 @@
+let x = 10
+const y = 20
+var z = 30
+
+if (true) {
+    let x = 10 //  it local variable can't access cause it is in scopes{} 
+    const y = 20   //  it local variable can't access cause it is in scopes{} 
+    var z = 30   //  it globle variable may access and this is disadvantage
+    
+}
+// console.log(a);
+// console.log(b);
+// console.log(c);
+
+//.................................................................
+
+
 //var c = 300
 let a = 300
 if (true) {
@@ -7,41 +24,47 @@ if (true) {
     
 }
 
-
-
 // console.log(a);
 // console.log(b);
 // console.log(c);
+
+//...................................................................
 
 
 function one(){
     const username = "hitesh"
 
     function two(){
-        const website = "youtube"
-        console.log(username);
+        const website = "youtube"    //
+        console.log(username);       /// Can access parent variable data (IseCream)
     }
-    // console.log(website);
+    // console.log(website);-----> can't acces childe variable data 
 
      two()
 
-}
+}         // one()
 
-// one()
+//...................................................................................
+
+
 
 if (true) {
     const username = "hitesh"
     if (username === "hitesh") {
         const website = " youtube"
-        // console.log(username + website);
+        // console.log(username + website);   Can access parent variable data (IseCream) cause username = "hitesh" is global for inner if(){...}
+        
     }
-    // console.log(website);
+    // console.log(website);  --->  can't acces childe variable data cause website = " youtube" is local for outer if(){...}
 }
 
-// console.log(username);
+// console.log(username);    // Can access parent variable data (IseCream) cause username = "hitesh" is local for inner if(){...}
 
 
-// ++++++++++++++++++ interesting ++++++++++++++++++
+
+
+
+// ++++++++++++++++++ interesting ++++++++++++++++++++++++++++++++++++++++++++++++++++
 
 
 console.log(addone(5))
@@ -52,7 +75,8 @@ function addone(num){
 
 
 
-addTwo(5)
-const addTwo = function(num){
+addTwo(5) // error upper
+const addTwo = function(num){  /// const addTwo it is a expration (varable type function)
     return num + 2
 }
+addTwo(5) // not error bottum
