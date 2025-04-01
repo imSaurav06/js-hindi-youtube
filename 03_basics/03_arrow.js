a58ea802e3a1adedf01e9c3dd47d9e47840f71db
imSaurav06 <156805591+imSaurav06@users.noreply.github.com>
2025-04-02 00:47:09 +0530

    Update 03_arrow.js

--- 03_basics/03_arrow.js
+++ 03_basics/03_arrow.js
@@ -3,33 +3,38 @@ const user = {
     price: 999,
 
     welcomeMessage: function() {
-        console.log(`${this.username} , welcome to website`);
+        console.log(`${this.username} , welcome to website`);  // tish ->>> current context
         console.log(this);
     }
 
 }
 
-// user.welcomeMessage()
+// user.welcomeMessage()  output ->>>> "hitesh", welcome to website
 // user.username = "sam"
-// user.welcomeMessage()
+// user.welcomeMessage()   output ->>>> output ->>>> "hitesh", welcome to website
 
-// console.log(this);
+// console.log(this);  output ->>>>> empty {}object
+
+//  #Windows  is globel object in  Browser  (interview)
+
+
+//.........................................................................
 
 // function chai(){
 //     let username = "hitesh"
-//     console.log(this.username);
+//     console.log(this.username); // undefined
 // }
 
 // chai()
 
 // const chai = function () {
 //     let username = "hitesh"
-//     console.log(this.username);
+//     console.log(this.username);   // undefined
 // }
 
 const chai =  () => {
     let username = "hitesh"
-    console.log(this);
+    console.log(this);  // undefined //{}
 }
 
 
